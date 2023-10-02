2eec47bd83be31b183b8a906831dff861b5b0f7a
simon marchal <simon.marchal@epitech.eu>
2023-10-02 10:20:29 -0400

    update task01

--- task01.js
+++ task01.js
@@ -2,7 +2,7 @@ export function drawTriangle(height) {
     for (let i = 1; i <= height; i++) {
         let row = "";
 
-        for (let k = 1; k <= 2 * i - 1; k++) {
+        for (let k = 1; k <= i - 1; k++) {
             row += "$";
     }
     console.log(row);
