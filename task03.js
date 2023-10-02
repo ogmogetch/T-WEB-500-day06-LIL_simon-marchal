d3fd1e424db0fc089b057841c85600a6022a1fd5
simon marchal <simon.marchal@epitech.eu>
2023-10-02 10:48:09 -0400

    task03 update

--- task03.js
+++ task03.js
@@ -1,9 +1,13 @@
 export function countGs(str){
     let count = 0;
     for (let i = 0; i < str.length; i++){
-        if (str[i] == "G"){
+        if (str[i] === "G"){
             count++
         }
     }
-    console.log(count)
+    return count
 }
+
+
+
+
