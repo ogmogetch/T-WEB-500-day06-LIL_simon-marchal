e0a8f881fc495ab30ebd01c3815720f2690e5540
simon marchal <simon.marchal@epitech.eu>
2023-10-02 05:49:18 -0400

    task05 update

--- task04.js
+++ task04.js
@@ -1,3 +1,15 @@
-export function fizzBuzz(num){
-    
+export function fizzBuzz(num) {
+    const tab = []
+    for (let i = 1; i <= num; i++) {
+        if (i % 3 === 0 && i % 5 === 0) {
+            tab.push("FizzBuzz");
+        } else if (i % 3 === 0) {
+            tab.push("Fizz");
+        } else if (i % 5 === 0) {
+            tab.push("Buzz");
+        } else {
+            tab.push(i);
+        }
+    }
+    return tab.join(", ")
 }
